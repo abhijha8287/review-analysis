11bb4f939eb6969f184901363a252e47ee52944b
abhishek <abhijha8287@gmail.com>
2025-06-11 17:59:41 +0530

    first commit

---
+++ withstrucuteoutput.py
@@ -0,0 +1,29 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+load_dotenv()
+from typing import TypedDict, Annotated, Optional
+import streamlit as st
+
+model=ChatOpenAI(temperature=0.9)
+ 
+st.title("Review Analysis")
+class Review(TypedDict):
+    key_theme: Annotated[list[str], "Key theme of the review"]
+    Summary: Annotated[str, "Summary of the review"]
+    sentiment: Annotated[str, "Sentiment of the review"]
+    pros=Annotated[Optional[list[str]], "Pros of the review"]
+    cons=Annotated[Optional[list[str]], "Cons of the review"]
+
+structured_model = model.with_structured_output(Review)
+
+review=st.text_input("Enter your review")
+
+
+
+if st.button('Summarize'):
+    # ✅ Send the final string to the model
+    
+    result = structured_model.invoke(review)
+
+    # ✅ Show model output
+    st.write(result)
